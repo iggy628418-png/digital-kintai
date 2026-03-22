036d495d766be57e9355d5b6646e23a9dfb371c9
koseki <wakamatuya.k@gmail.com>
2026-03-22 15:43:00 +0900

    Security: Move Firebase config to environment variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbmDtuDUu8UoMW7K0uKCIgeFY7EXkDR6k",
-  authDomain: "digital-kintai.firebaseapp.com",
-  projectId: "digital-kintai",
-  storageBucket: "digital-kintai.firebasestorage.app",
-  messagingSenderId: "304323604583",
-  appId: "1:304323604583:web:bd5b00730aa261ea7024c2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
